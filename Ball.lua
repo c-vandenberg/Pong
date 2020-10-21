f409965671cfe08591d98626dffa9ee55dd5d64c
Christopher van den Berg <60201356+c-vandenberg@users.noreply.github.com>
2020-10-21 18:19:34 +0100

    Update Ball.lua

--- Ball.lua
+++ Ball.lua
@@ -9,7 +9,9 @@ function Ball:init(x, y, width, height)
     self.y = y
     self.width = width
     self.height = height
-
+    
+    -- Keep track of velocity on both the x and y axis
+    -- and or statement is ternary operator (i.e if math.random(2) == 1 Boolean is true, ballDX = -100, or left. But if math.random(2) == 1 Boolean is flase, ballDX = 100, or right)
     self.dx = math.random(2) == 1 and 100 or -100
     self.dy =  math.random(-50, 50)
 end
@@ -24,7 +26,6 @@ function Ball:reset()
     self.y = VIRTUAL_HEIGHT / 2 - 2
 
     -- Ball velocity in x-direction (randomise for left or right direction for beginning of match)
-    -- and or statement is ternary operator (i.e if math.random(2) == 1 Boolean is true, ballDX = -100, or left. But if math.random(2) == 1 Boolean is flase, ballDX = 100, or right)
     self.dx = math.random(2) == 1 and 100 or -100
 
     -- Ball velocity in y-direction (random range of values between -50 and 50)
